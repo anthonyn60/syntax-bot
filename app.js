9de8ae2ba8c036c6dd9f021cfd730ff8cc85e92b
anthonyn60 <anthony_nguyen@live.ca>
2016-05-15 20:15:58 -0400

    Changed to NLP dialogs

--- app.js
+++ app.js
@@ -3,71 +3,38 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=f7fce781-ff77-4403-8c45-b5d3f5773643&subscription-key=6bb0629d272d45ab917873b75316b07a';
+var dialog = new builder.LuisDialog(model);
 var syntaxBot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
-syntaxBot.add('/', new builder.CommandDialog()
-    .matches('link', function(session) {
-        if(session.userData.concept) sendLink(session);
-        else {
-            session.send("Sorry, you haven't asked for any syntax to link to. If you're looking for a link to SyntaxDB, here it is: https://syntaxdb.com");
-        }
-    })
-    .matches('example|sample', function(session){
-        if(session.userData.concept) sendExample(session);  
-        else {
-            session.send("Sorry, you haven't asked for any syntax so I can't provide you with an example.");
-            builder.DialogAction.beginDialog('/getSyntax');
-        }
-    })
-    .matches("language|another language|different language",
-          [function(session) {  
-                builder.Prompts.text(session, 'What language would you like to switch to?');
-          }, function(session, results){
-                var apiLink = "https://syntaxdb.com/api/v1/concepts/search?q=" + encodeURIComponent(session.userData.concept.concept_name + " " + results.response).toString();
-                findConcept(apiLink, session);
-          }]
-    )
-    .matches("other|another|different|something else", function(session){
-        if(session.userData.allConcepts != null && session.userData.current < session.userData.allConcepts.length - 1) {
-            session.userData.concept = session.userData.allConcepts[session.userData.current + 1];
-            session.send("Okay, here's another. This is the syntax for " + session.userData.concept.concept_search + ":\n\n" + session.userData.concept.syntax);
-            session.userData.current += 1;
-        }
-        else if(session.userData.concepts != null || session.userData.concept) {
-            session.send("Sorry, I've run out of results for you.");
-        } else {
-            session.send("Sorry, I can't give you results if you haven't provided me with syntax. Type 'syntax' to get started.")
-        }
-    })
-    .matches('syntax', builder.DialogAction.beginDialog('/getSyntax'))
-    .matches('help', function(session) {
-        if(session.userData.concept) session.send("Type 'example' to get an example, 'link' to get a link to the concept, or 'another' to get a different result.\n\n You can also search for something else by typing 'syntax'.");
-        else session.send("Type 'syntax' to begin.");
-    })
-    .onDefault(function(session) {
-        if(session.userData.concept) session.send('You can ask for an example or a link to the page, or you can ask for another concept by asking for syntax.');
-        else session.send('Hey there, I\'m the syntax box! Ask me for syntax to begin.');
-    })
-);
 
-syntaxBot.add('/getSyntax', [
-    function(session) {
-        builder.Prompts.text(session, 'What syntax are you looking for?');
+syntaxBot.add('/', dialog);
+dialog.on('SyntaxLookupActivity', [
+    function(session, args, next){
+        var concept = builder.EntityRecognizer.findEntity(args.entities, 'concept');
+        var language = builder.EntityRecognizer.findEntity(args.entities, 'language');
+        if(concept && language) {
+            session.userData.syntaxQuery = concept.entity + " " + language.entity;
+            next();
+        } else if(concept) {
+            session.userData.queryConcept = concept.entity;
+            builder.Prompts.text(session, 'Which language would you like to search \'' + concept.entity + '\' in?');
+        } else if(language) {
+            session.userData.queryLanguage = language.entity;
+            builder.Prompts.text(session, 'Which concept would you like to search in ' + language.entity + '?');
+        } else {
+            builder.Prompts.text(session, "Sure, what syntax are you looking for?");
+        };
     },
     function(session, results) {
-        session.userData.syntaxQuery = results.response;
+        if(results.response){
+            if(session.userData.queryConcept) session.userData.syntaxQuery = results.response + " " + session.userData.queryConcept;
+            else if(session.userData.queryLanguage) session.userData.syntaxQuery = session.userData.queryConcept + " " + results.response;
+            else session.userData.syntaxQuery = results.response;     
+        }
         var apiLink = "https://syntaxdb.com/api/v1/concepts/search?q=" + encodeURIComponent(session.userData.syntaxQuery).toString();
         findConcept(apiLink, session);
     }
-]);
-
-
-function sendLink(session){
-    session.send("Okay, here's the link: " + "https://syntaxdb.com/ref/" + session.userData.concept.language_permalink + '/' + session.userData.concept.concept_permalink);
-}
-
-function sendExample(session){
-    session.send("Sure, here's an example:\n\n" + session.userData.concept.example);
-}
+])
 
 function findConcept(apiLink, session) {
     https.get(apiLink, function(response) {
@@ -84,7 +51,7 @@ function findConcept(apiLink, session) {
                             }
                             session.userData.concept = concepts[0];
                             session.send("Here's the " + concepts[0].concept_search + " syntax: \n\n" + concepts[0].syntax);
-                            session.endDialog();
+                            //session.endDialog();
                         } else {
                             var soLink = "http://stackoverflow.com/search?q=" + encodeURIComponent(session.userData.syntaxQuery).toString();
                             session.send("Sorry, I could find the syntax for what you just searched. Hopefully I'll be able to some time in the near future. In the meantime, here's a link to the same search on StackOverflow: " + soLink);
@@ -92,6 +59,61 @@ function findConcept(apiLink, session) {
                     })
                 });
 }
+
+dialog.on('ChangeLanguageActivity', [
+          function(session, args, next){
+                if(session.userData.concept) {
+                    var language = builder.EntityRecognizer.findEntity(args.entities, 'language');
+                    if(language) {
+                        session.userData.language = language.entity;
+                        next();
+                    }
+                    else {
+                        builder.Prompts.text(session, 'What language would you like to switch to?');
+                    }
+                } else {
+                    session.send("Sorry, I can't switch languages if there's no concept to switch for.");
+                }
+          }, function(session, results){
+                if(results.response) session.userData.language = results.response;
+                session.userData.syntaxQuery = session.userData.concept.concept_name + " " + session.userData.language;
+                var apiLink = "https://syntaxdb.com/api/v1/concepts/search?q=" + encodeURIComponent(session.userData.syntaxQuery).toString();
+                findConcept(apiLink, session);
+          }
+])
+
+dialog.on('ExampleActivity', function(session){
+        if(session.userData.concept) session.send("Sure, here's an example:\n\n" + session.userData.concept.example);  
+        else {
+            session.send("Sorry, you haven't asked for any syntax so I can't provide you with an example.");
+        }
+})
+
+dialog.on('LinkActivity', function(session){
+        if(session.userData.concept) session.send("Okay, here's the link: " + "https://syntaxdb.com/ref/" + session.userData.concept.language_permalink + '/' + session.userData.concept.concept_permalink);
+        else {
+            session.send("Sorry, you haven't asked for any syntax to link to. If you're looking for a link to SyntaxDB, here it is: https://syntaxdb.com");
+        }
+})
+
+dialog.on('DifferentConceptActivity', function(session){
+     if(session.userData.allConcepts != null && session.userData.current < session.userData.allConcepts.length - 1) {
+            session.userData.concept = session.userData.allConcepts[session.userData.current + 1];
+            session.send("Okay, here's another. This is the syntax for " + session.userData.concept.concept_search + ":\n\n");
+            session.send(session.userData.concept.syntax);
+            session.userData.current += 1;
+        }
+        else if(session.userData.concepts != null || session.userData.concept) {
+            session.send("Sorry, I've run out of results for you.");
+        } else {
+            session.send("Sorry, I can't give you results if you haven't provided me with syntax. Type 'syntax' to get started.")
+        }
+})
+
+dialog.onDefault(function(session){
+        if(session.userData.concept) session.send('You can ask for an example or a link to the page, or you can ask for another concept by asking for syntax.');
+        else session.send('Hey there, I\'m the syntax box! Ask me for syntax to begin.');
+})
 
 // Setup Restify Server
 var server = restify.createServer();
